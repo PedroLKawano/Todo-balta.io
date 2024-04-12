b69d234e7a0e2021a306a38a9e6d2cb6576d4ae1
Pedro Kawano <pedrokawano@outlook.com>
2024-04-11 23:48:16 -0300

    adiciona metodos getbyid, put e delete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,14 @@ namespace Todo.Controllers
             return context.Todos.ToList();
         }
 
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id);
+        }
+
         [HttpPost("/")]
         public TodoModel Post(
             [FromBody] TodoModel todo,
@@ -22,6 +30,35 @@ namespace Todo.Controllers
             context.SaveChanges();
 
             return todo;
+        }
+
+        [HttpPut("/{id:int}")]
+        public TodoModel Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return todo;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+            return model;
+        }
+
+        [HttpPost("/{id:int}")]
+        public TodoModel Delete(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            context.Todos.Remove(model);
+            context.SaveChanges();
+            return model;
         }
     }
 }
